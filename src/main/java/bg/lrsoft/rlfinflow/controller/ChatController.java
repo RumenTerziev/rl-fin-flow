45d7ba7c3ea3099a0d41b56542eadee942fb425b
Rumen Terziev <106109989+RumenTerziev@users.noreply.github.com>
2025-06-15 17:21:28 +0300

    Updated endpoints. Modified properties.

--- src/main/java/bg/lrsoft/rlfinflow/controller/ChatController.java
+++ src/main/java/bg/lrsoft/rlfinflow/controller/ChatController.java
@@ -2,7 +2,13 @@ package bg.lrsoft.rlfinflow.controller;
 
 import bg.lrsoft.rlfinflow.domain.dto.ChatMessageRequestDto;
 import bg.lrsoft.rlfinflow.domain.dto.ChatMessageResponseDto;
-import bg.lrsoft.rlfinflow.service.ChatAiService;
+import bg.lrsoft.rlfinflow.domain.dto.CurrencyResponseDto;
+import bg.lrsoft.rlfinflow.domain.dto.ErrorPayloadDto;
+import bg.lrsoft.rlfinflow.service.AssistantService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,13 +17,39 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/chat")
+@RequestMapping("/assistant")
 public class ChatController {
 
-    private final ChatAiService chatAiService;
+    private final AssistantService assistantService;
 
-    @PostMapping("/ask-ai")
-    public ChatMessageResponseDto askAi(@RequestBody ChatMessageRequestDto prompt) {
-        return chatAiService.getChatResponseFromAi(prompt);
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Success",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CurrencyResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad Request",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorPayloadDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorPayloadDto.class)
+                    )
+            )
+    })
+    @PostMapping("/chat")
+    public ChatMessageResponseDto chatWithAi(@RequestBody ChatMessageRequestDto prompt) {
+        return assistantService.getChatResponseFromAi(prompt);
     }
 }
